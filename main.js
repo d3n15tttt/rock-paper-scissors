68319760ec322a395dd786d4851a8d6676a8116f
d3n15tttt <106825618+d3n15tttt@users.noreply.github.com>
2025-04-27 00:31:28 +0300

    add comments

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-function getComputerChoice(){
-    let x = Math.floor(Math.random() * 3) + 1;
-    if (x === 1) {
+// get computer choice
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3) + 1; // randomize a number from 1 to 3 (1 = rock, 2 = paper, 3 or everything else = scissors)
+    if (x === 1) { // 1 = rock
         return "rock";
-    }
-    else if (x === 2) {
+    } else if (x === 2) { // 2 = paper
         return "paper";
-    }
-    else {
-        return "scissors";
+    } else {            // everything else = scissors
+        return "scissors"; 
     }
 }
 
 function getHumanChoice() {
-    return prompt("ROCK, PAPER, SCISSORS").toLowerCase();
+    return prompt(`ROCK, PAPER OR SCISSORS?`);
 }
 
 let humanScore;
@@ -22,24 +21,22 @@ function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
 
     if (humanChoice === computerChoice) {
-        console.log("tie");
+        console.log(`it's a tie, you both picked ${humanChoice}`);
         return;
     }
 
-    const winConditions = {
+    let winConditions = {
         rock:       "scissors",
         paper:      "rock",
         scissors:   "paper"
     }
 
-    if (winConditions[humanChoice] === computerChoice){
+    if (winConditions[humanChoice] === computerChoice) {
         humanScore++;
-        console.log(`you win, you had ${humanChoice}, computer had ${computerChoice}.`);
-        
+        console.log(`You won, you picked ${humanChoice}, while COM picked ${computerChoice}.`);
     } else {
         computerScore++;
-        console.log(`you lost, you had ${humanChoice}, computer had ${computerChoice}.`);
-        
+        console.log(`You faken lost mate, you had ${humanChoice}, while the COM had ${computerChoice}.`);
     }
 }
 
@@ -48,23 +45,20 @@ function playGame() {
     computerScore = 0;
 
     for (let round = 1; round <= 5; round++) {
-        console.log(`-------ROUND: ${round} --------`)
+        console.log(`-----ROUND:${round}-----`);
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
-        console.log(`SCORE:----YOU:${humanScore}----COMPUTER:${computerScore}`);
+        console.log(`SCORE: YOU - ${humanScore} -- COM - ${computerScore}`);
     }
 
     if (humanScore > computerScore) {
-        console.log(`you win congrats, you had ${humanScore}, com had ${computerScore}`);
+        console.log(`you won the whole game congrats, final score: you-${humanScore} com-${computerScore}`);
+    } else if (computerScore > humanScore) {
+        console.log(`you lost the whole game wcyd, final score: you-${humanScore} com-${computerScore}`);
+    } else {
+        console.log(`it's a draw bruh, final score: you-${humanScore} com-${computerScore}`);
     }
-    else if (computerScore > humanScore) {
-        console.log(`you faken lost mate, you had ${humanScore}, com had ${computerScore}`);
-    }
-    else {
-        console.log("tie");
-    }
-
 }
 
 playGame();
